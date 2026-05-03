d54dce7bc268b0cbc67e8899a2f13b385b0d866c
nahuel1111 <ricardo-nahuel@hotmail.com>
2026-05-03 19:45:27 -0300

    se realizo el ejercicio 5 con exito

--- Dsw2026Ej8/Problema4.cs
+++ Dsw2026Ej8/Problema4.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace Dsw2026Ej8
 {
-    internal class Problema4
+    public class Problema4
     {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidad = 0;
+
+            void ProcesarNota(int? nota)
+            {
+                if (nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    suma += nota.Value;
+                    cantidad++;
+                }
+            }
+
+            ProcesarNota(nota1);
+            ProcesarNota(nota2);
+            ProcesarNota(nota3);
+
+            if (cantidad == 0)
+                return 0;
+
+            return (double)suma / cantidad;
+        }
     }
 }
