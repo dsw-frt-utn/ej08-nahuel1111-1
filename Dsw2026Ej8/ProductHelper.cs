019514274b2995cede35124cd79ec5dbf285d9b7
nahuel1111 <ricardo-nahuel@hotmail.com>
2026-05-03 19:32:16 -0300

    se realizo el ejercicio 1 con exito

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            return $"[{code}] {description} - {FormatoMoneda(price)}";
+        }
+
+    }
+}
---
+++ Dsw2026Ej8/ProductHelperExtra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        private string FormatoMoneda(decimal price)
+        {
+            return price.ToString("C", new System.Globalization.CultureInfo("es-AR"));
+        }
+    }
+}
